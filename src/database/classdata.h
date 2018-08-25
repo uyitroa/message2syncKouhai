30861daf558b56670342397334b0e3eda3aeed50
yuitora <30695483+shintaroDesu@users.noreply.github.com>
2018-08-25 23:58:18 +0200

    updatae classdta.h

--- src/database/classdata.h
+++ src/database/classdata.h
@@ -1,9 +1,13 @@
 #ifndef SRC_DATABASE_CLASSDATA_H_
 #define SRC_DATABASE_CLASSDATA_H_
+
 #include <iostream>
 #include <vector>
+
 #include "../abstracts/ACommand.h"
 #include "../commands/Bash/Bash.h"
 #include "../commands/Send/Send.h"
+
 std::vector<ACommand*> command_list = {new Bash, new Send};
+
 #endif
