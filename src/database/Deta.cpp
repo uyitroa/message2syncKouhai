65dccaf39b1052d47f804580956668848aed20de
yuitora <30695483+shintaroDesu@users.noreply.github.com>
2018-08-25 13:41:05 +0200

    update Deta class

--- src/database/Deta.cpp
+++ src/database/Deta.cpp
@@ -7,10 +7,33 @@
 
 #include "Deta.h"
 
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
+
 Deta::Deta(std::string address, std::string name, std::string password) {
 	this->driver = get_driver_instance();
 	this->con = driver->connect(address, name, password);
-	this->con->setSchema("mydatabase");
+
+	try {
+		this->con->setSchema(this->DATABASENAME);
+
+	} catch (sql::SQLException &e) {
+		// if the database is not created
+		if(e.what() == this->NODATABASE) {
+			sql::Statement *stmt = this->con->createStatement();
+			stmt->execute("CREATE DATABSE " + this->DATABASENAME);
+			this->con->setSchema(this->DATABASENAME);
+			stmt->execute("CREATE TABLE classes (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(40), path VARCHAR(200), created DATE, PRIMARY KEY(id))");
+
+			// check if column amount exist for readClass, which need a vector, and if we know already the amount, no need for push_back();
+			stmt->execute("CREATE TABLE classes_amount (id INT NOT NULL AUTO_INCREMENT, amount INT, PRIMARY KEY(id))");
+			stmt->execute("INSERT INTO classes_amount (amount) VALUES (0)");
+			delete stmt;
+		}
+	}
+
 }
 
 Deta::Deta() { // @suppress("Class members should be properly initialized")
@@ -23,6 +46,7 @@ Deta::~Deta() {
 
 void Deta::createClass(std::string name, std::string path) {
 	sql::PreparedStatement *prep_stmt;
+
 	prep_stmt = this->con->prepareStatement("INSERT INTO classes(name, path, created) VALUES(?, ?, NOW())");
 
 	prep_stmt->setString(1, name);
@@ -31,4 +55,28 @@ void Deta::createClass(std::string name, std::string path) {
 	prep_stmt->execute();
 
 	delete prep_stmt;
+}
+
+void Deta::readClass(std::vector<std::string>& names,
+		std::vector<std::string>& paths) {
+	sql::Statement *stmt = this->con->createStatement();
+	sql::ResultSet *res;
+
+	res = stmt->executeQuery("SELECT * from classes_amount");
+	int size = res->getInt(2);
+
+	names.reserve(size);
+	paths.reserve(size);
+
+	res = stmt->executeQuery("SELECT * from classes");
+	int index = 0;
+	while(res->next()) {
+		names[index] = res->getString(2);
+		paths[index] = res->getString(3);
+		index++;
+	}
+
+	delete res;
+	delete stmt;
+
 }
--- src/database/Deta.h
+++ src/database/Deta.h
@@ -9,19 +9,21 @@
 #define SRC_DATABASE_DETA_H_
 
 #include <iostream>
+#include <vector>
 #include <mysql.h>
 
 #include <mysql_connection.h>
 #include <cppconn/driver.h>
-#include <cppconn/exception.h>
-#include <cppconn/resultset.h>
-#include <cppconn/statement.h>
-#include <cppconn/prepared_statement.h>
+
 
 class Deta {
 private:
 	sql::Driver *driver;
 	sql::Connection *con;
+
+	const std::string DATABASENAME = "mydatabase";
+	const std::string NODATABASE = "Unknown database '" + this->DATABASENAME + "'";
+	const std::string NOFIELDAMOUNT = " Table '" + this->DATABASENAME + ".amount' doesn't exist";
 
 public:
 	Deta(std::string address, std::string name, std::string password);
@@ -30,7 +32,8 @@ public:
 
 	void createClass(std::string name, std::string path);
 
-
+	void readClass(std::vector<std::string> &names,
+			std::vector<std::string> &paths);
 };
 
 #endif /* SRC_DATABASE_DETA_H_ */
