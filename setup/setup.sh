48af385ef9373c3bade80ce1ecb9734401a2815e
yuitora <yuitora.@yuitora-2.local>
2018-09-01 17:33:24 +0200

    isntall mysql

---
+++ setup/setup.sh
@@ -0,0 +1,4 @@
+ln -s /usr/local/mysql-connector-c++-8.0/lib64/libmysqlcppconn.7.dylib /usr/local/lib/libmysqlcppconn.7.dylib
+ln -s /usr/local/mysql/lib/libssl.1.0.0.dylib /usr/local/lib/libssl.1.0.0.dylib
+ln -s /usr/local/mysql/lib/libcrypto.1.0.0.dylib /usr/local/lib/libcrypto.1.0.0.dylib
+echo DONE
