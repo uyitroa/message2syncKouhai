ae51ec62c8214e54fcc7e6e0228e6d58b0f08b85
shintaroDesu <snkraishin87@gmail.com>
2018-10-06 22:57:40 +0200

    add sendrequest for client side

--- src/Senpai/client/SendRequest.cpp
+++ src/Senpai/client/SendRequest.cpp
@@ -1,5 +1,40 @@
+#include <utility>
+
 //
 // Created by yuitora . on 06/10/2018.
 //
 
 #include "SendRequest.h"
+#include <istream>
+#include <ostream>
+#include <string>
+#include <sstream>
+//
+SendRequest::SendRequest(std::string host, std::string port) {
+	this->host = std::move(host);
+	this->port = std::move(port);
+}
+
+void SendRequest::sendpost(std::string json, std::string host_name, std::string path) {
+	boost::asio::io_service io_service;
+
+	tcp::resolver resolver(io_service);
+	tcp::resolver::query query(host, port);
+	tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+
+	tcp::socket socket_(io_service);
+	boost::asio::connect(socket_, endpoint_iterator);
+
+	boost::asio::streambuf request;
+	std::ostream request_stream(&request);
+
+	request_stream << "POST " << path << " HTTP/1.1 \r\n";
+	request_stream << "Host: " << host_name << "\r\n";
+	request_stream << "Content-Type: application/json; charset=utf-8\r\n";
+	request_stream << "Accept: application/json\r\n";
+	request_stream << "Content-Length: " << json.length() << "\r\n\r\n";
+	request_stream << json << "\n\n";
+
+	boost::asio::write(socket_, request);
+
+}
--- src/Senpai/client/SendRequest.h
+++ src/Senpai/client/SendRequest.h
@@ -2,15 +2,23 @@
 // Created by yuitora . on 06/10/2018.
 //
 
-#ifndef MESSAGE2SYNC_SENDREQUEST_H
-#define MESSAGE2SYNC_SENDREQUEST_H
+#ifndef SRC_SENPAI_SENDREQUEST_H
+#define SRC_SENPAI_SENDREQUEST_H
 
+#include <iostream>
+#include <boost/asio.hpp>
 
+using boost::asio::ip::tcp;
 
 class SendRequest {
+private:
+	std::string host;
+	std::string port;
 
+public:
+	SendRequest(std::string host = "127.0.0.1", std::string port = "8000");
+	void sendpost(std::string json, std::string host_name = "127.0.0.1:8000", std::string path = "/receive/");
 };
-
 
 
 #endif //MESSAGE2SYNC_SENDREQUEST_H
