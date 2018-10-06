2947b1811e270fcccf6c4b150da2b717c896a4f7
shintaroDesu <snkraishin87@gmail.com>
2018-10-06 23:01:29 +0200

    add installing stuff in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+pip3 install django
+brew install boost
+
 string=$'#ifndef MESSAGE2SYNC_DATAPATH_H\n#define MESSAGE2SYNC_DATAPATH_H\n#include <iostream>\n\nstatic std::string filepath="'
 string=$string`pwd`"/"
 string=$string$'";\n\n#endif //MESSAGE2SYNC_DATAPATH_H'
