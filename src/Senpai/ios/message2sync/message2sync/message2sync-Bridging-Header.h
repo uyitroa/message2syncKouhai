41b8c6f38eb18b792173445c87b83320cc79b4ec
shintaroDesu <snkraishin87@gmail.com>
2018-10-20 19:02:04 +0200

    divide project into two projects

--- src/Senpai/ios/message2sync/message2sync/message2sync-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  message2sync-Bridging-Header.h
-//  message2sync
-//
-//  Created by yuitora . on 20/10/2018.
-//  Copyright © 2018 yuitora . All rights reserved.
-//
-
-#include "bridger/wrapper.h"
