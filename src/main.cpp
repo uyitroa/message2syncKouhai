6149c2763a587014b8b7afc0d82ea1c085d3c90d
shintaroDesu <snkraishin87@gmail.com>
2018-10-20 19:14:01 +0200

    update stuff

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,9 @@
 #include "Kouhai/core/Koneku.h"
 #include "Kouhai/database/Deta.h"
 #include "Kouhai/database/datapath.h"
-void input();
 
-void runserver() {
-	std::string command = "python3 " + filepath + "src/Kouhai/server/manage.py runserver 0.0.0.0:8000 &";
-	system(command.c_str());
-}
-void controller() {
-	int choice;
-	std::cout << "1.Run   2.Add class: ";
-	std::cin >> choice;
-
-	if(choice == 1) {
+void controller(char choice) {
+	if(choice == '1') {
 		Koneku my_app;
 		runserver();
 		my_app.launch();
@@ -26,17 +17,7 @@ void controller() {
 	}
 }
 
-void commander() {
-	input();
-}
-
-
-
 int main(int argc, char *argv[]) {
-	if(argv[1][0] == '1') {
-		controller();
-	} else if (argv[1][0] == '2') {
-		commander();
-	}
+	controller(argv[1][0]);
 	return 0;
 }
