93dd112687d0993c02366307f4f59900346f4de7
shintaroDesu <snkraishin87@gmail.com>
2018-10-27 20:20:00 +0200

    minor update

---
+++ stopserver.sh
@@ -0,0 +1,2 @@
+ps -ax | grep "/Users/yuitora./message2syncKouhai/src/server/manage.py runserver" | xargs kill
+ps -ax | grep "/Users/yuitora./message2syncKouhai/src/server/manage.py runserver" | xargs kill
